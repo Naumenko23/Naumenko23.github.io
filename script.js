74667d48ce89ad91d9bade19a91dc4fecbeb1f9b
Naumenko23 <90448800+Naumenko23@users.noreply.github.com>
2021-10-12 18:32:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function script1(){
     let re = /^\d+$/;
-    let k1 = document.getElementById("field1");
-    let p1 = document.getElementById("field2");
+    let k1 = document.getElementById("kol");
+    let p1 = document.getElementById("price");
     let r = document.getElementById("result");
     if(re.test(k1.value) && re.test(p1.value)){
       r.innerHTML = parseInt(k1.value) * parseInt(p1.value);
