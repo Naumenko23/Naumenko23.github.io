b67fe4fe79bdc52688036791a993b6cba3a38344
Naumenko23 <90448800+Naumenko23@users.noreply.github.com>
2021-10-12 18:29:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function click1(){
+function script1(){
     let re = /^\d+$/;
-    let f1 = document.getElementById("field1");
-    let f2 = document.getElementById("field2");
+    let k1 = document.getElementById("field1");
+    let p1 = document.getElementById("field2");
     let r = document.getElementById("result");
-    if(re.test(f1.value) && re.test(f2.value)){
-      r.innerHTML = parseInt(f1.value) * parseInt(f2.value);
+    if(re.test(k1.value) && re.test(p1.value)){
+      r.innerHTML = parseInt(k1.value) * parseInt(p1.value);
     } else{
       r.innerHTML = "Введите корректные данные";
     }
@@ -13,5 +13,5 @@ function click1(){
 
   window.addEventListener('DOMContentLoaded', function (event) {
     console.log("DOM fully loaded and parsed");
-    document.getElementById("button1").addEventListener("click", click1);
+    document.getElementById("button1").addEventListener("click", script1);
   });
